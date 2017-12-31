54036ef3bac46628c9be33c67f56a044e0b78395
Cris <crislaney@gmail.com>
2017-12-31 14:31:51 -0800

    Mostly working interpreter returns an error on last step for some reason

--- classes/LiveUpdater.py
+++ classes/LiveUpdater.py
@@ -10,11 +10,18 @@ class LiveUpdater():
         self.phil_lights = PhilipsLights()
 
 
+    def load_script(self):
+
+        return
+
+
     def all_on(self):
         self.phil_lights.all_on()
 
+
     def all_off(self):
         self.phil_lights.all_off()
+
 
     # need to somehow determine what objects will be activated here
     def create_step(self, step_time=0, obj_index=None):
@@ -28,9 +35,92 @@ class LiveUpdater():
 
 
     def run_script(self, script):
-        # interpretation here
-        for step in script:
-            self.phil_lights.run_step(step)
+        self.interpret_script(script, 0, -1, -1, -1)
+
+    '''
+    def interpret_script(self, script, step_num, start_for, end_loop, cond):
+        if step_num >= len(script):
+            return
+        elif 'start_loop' in script[step_num]:
+            if cond < 0:
+                self.interpret_script(script, step_num+1, step_num, -1, \
+                script[step_num]['start_loop'])
+            elif cond > 0:
+                self.interpret_script(script, step_num + 1, step_num, end_loop, \
+                cond -1)
+                step_num += 1
+            elif cond == 0:
+                return
+                # self.interpret_script(script, end_loop + 1, -1, -1, -1)
+        elif 'end_loop' in script[step_num]:
+            self.interpret_script(script, start_for, start_for, step_num, cond)
+        else:
+            self.phil_lights.run_step(script[step_num])
+            step_num += 1
+
+        print(step_num)
+        self.interpret_script(script, step_num, start_for, end_loop, cond)
+    '''
+
+    def interpret_script(self, script, step_num, start_loop, end_loop, cond):
+        while step_num < len(script):
+            if 'start_loop' in script[step_num]:
+                print('found start_loop')
+                if step_num != start_loop:
+                    print('recursing')
+                    self.interpret_script(self, script, step_num, step_num, \
+                    -1, script[step_num]['start_loop'])
+                if cond < 0:
+                    print('error')
+                    # this should probs be an error case???
+                    return end_loop
+                elif cond == 0:
+                    print('returning from recursion')
+                    return end_loop
+                else:
+                    print('iterating in recursive call')
+                    cond -= 1
+                    step_num += 1
+            elif 'end_loop' in script[step_num]:
+                print('found end_loop')
+                end_loop = step_num
+                step_num = start_loop
+            else:
+                print('executing step')
+                # self.run_step(script[step_num])
+                step_num += 1
+
+
 
     def get_phil_lights(self):
         return self.phil_lights
+
+def interpret_script(script, step_num, start_loop, end_loop, cond):
+    while step_num < len(script):
+        if 'start_loop' in script[step_num]:
+            print('found start_loop')
+            if step_num != start_loop:
+                print('recursing')
+                step_num = interpret_script(script, step_num, step_num, \
+                -1, script[step_num]['start_loop']) + 1
+                print('result from recursion ', step_num)
+            if cond < 0:
+                print(cond)
+                print('error')
+                # this should probs be an error case???
+                return end_loop
+            elif cond == 0:
+                print('returning from recursion')
+                return end_loop
+            else:
+                print('iterating in recursive call')
+                cond -= 1
+                step_num += 1
+        elif 'end_loop' in script[step_num]:
+            print('found end_loop')
+            end_loop = step_num
+            step_num = start_loop
+        else:
+            print('executing step')
+            # self.run_step(script[step_num])
+            step_num += 1
--- classes/test_classes.py
+++ classes/test_classes.py
@@ -1,14 +1,16 @@
 
 from LiveUpdater import LiveUpdater
+from LiveUpdater import interpret_script
 from PhilipsLights import PhilipsLights
 import time
 
 
 def main():
     print("Starting test program")
+    '''
     updater = LiveUpdater()
 
-    '''
+    updater.all_off()
     updater.set_red()
     step1 = updater.create_step(5, [4])
     time.sleep(1)
@@ -27,11 +29,13 @@ def main():
     script = []
     script.append({4:{'hue':0, 'sat':254, 'bri':254, 'transitiontime':50}})
     script.append({4:{'hue':20000, 'sat':254, 'bri':254, 'transitiontime':5}})
+    script.append({'start_loop':3})
     script.append({4:{'hue':20000, 'sat':254, 'bri':127, 'transitiontime':50}})
     script.append({4:{'hue':20000, 'sat':254, 'bri':254, 'transitiontime':5}})
+    script.append({'end_loop':0})
     script.append({4:{'hue':0, 'sat':254, 'bri':254, 'transitiontime':100}})
     script.append({4:{'hue':40000, 'sat':254, 'bri':254, 'transitiontime':5}})
 
-    updater.run_script(script)
+    interpret_script(script, 0, -1, -1, -1)
 
 main()
