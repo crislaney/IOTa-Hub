58f77569d79a2a5bbdfe55a9ede400cd711314ca
Cris <crislaney@gmail.com>
2018-03-08 16:02:10 -0800

    added endpoints

--- app.py
+++ app.py
@@ -42,12 +42,9 @@ def setup():
     return
 
 @app.route("/step", methods=['POST'])
-def build_step():
+def record_step():
+    updater.create_step()
 
-    return
-@app.route("/script", methods=['POST'])
-def start_script():
-    return
 
 if __name__=='__main__':
     app.run(debug=True)
